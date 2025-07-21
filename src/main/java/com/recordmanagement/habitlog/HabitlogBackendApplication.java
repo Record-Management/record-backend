c531deada6fcf5d33630d0845bb11eb272141934
전우선 <jeonwooseon@naver.com>
2025-07-21 09:31:02 +0900

    Git 초기화 및 기본 설정 파일 추가

---
+++ src/main/java/com/recordmanagement/habitlog/HabitlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recordmanagement.habitlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recordmanagement/habitlog/HabitlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordmanagement.habitlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
